467275c58de7ae0e960df42f34cf890404f3a81c
Stuart Dallas <stuart@stut.net>
2024-09-22 00:36:06 +0100

    Pulled the switch script from the server

---
+++ switch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+    echo "Usage: $0 server|client|hybrid"
+    exit 1
+fi
+
+# Use the hardware configuration created by the installer
+cp -f /etc/nixos/hardware-configuration.nix ./hardware-configuration.nix
+
+# Switch to the configuration specified by the argument
+sudo nixos-rebuild switch --upgrade --flake .#$1
