bb70a6b3214c9cd3926388250bc7e1f5bfc68fa8
Stuart Dallas <stuart@stut.net>
2024-12-20 01:04:54 +0000

    Added a script to apply changes to my dev cluster

---
+++ apply-remote.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+for host in s01 c01 c02 c03; do scp -r * ${host}:nixos-config/; done
+
+echo s01
+ssh s01 "cd nixos-config && ./switch.sh server"
+
+for host in c01 c02 c03; do
+	echo $host
+	ssh $host "cd nixos-config && ./switch.sh client"
+done
+
